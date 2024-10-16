dac647061fc4cea00e23b32ecd8ae0e9687e9e87
mutualadestruc <madpsychodelia@gmail.com>
2024-10-16 16:51:03 -0500

    instalacion de dos plugins de h5p y acualizacion en php.ini de upload_max file a 2000M

---
+++ moodledata/cache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'OuHObiXGVAaSMTcCAwNLMLwHCW9WeF1flocalhost';
+$CFG->bootstraphash = 'a5428a8ce7620a601b9c6d0a6c17c56a';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
